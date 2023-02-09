e762ff349cc4e410b3bd94b15debbeb47569a26f
sneharajpurkar <snehasrajpurkar@gmail.com>
2023-02-09 11:05:23 +0530

    first

--- Day10-javascript.js
+++ Day10-javascript.js
@@ -1,2 +1,37 @@
-var sneha = 1871;
-console.log(sneha);
+// var sneha = 1871;
+// console.log(sneha);
+// var string1 = "sneha";
+// var string2 = 'sneha';
+// var string3 = `sneha`;
+// console.log(string1);
+// console.log(string2);
+// console.log(string3);
+
+// var data = 18;
+// console.log("data :",data);
+// console.log("data type :",typeof(data));
+// var data2 = 'sneha';
+// console.log("data :",data2);
+// console.log("data type :",typeof(data2));
+// var data3 = false;
+// console.log("data :",data3);
+// console.log("data type :",typeof(data3));
+
+// a=96;
+// b=99
+
+// console.log(a>b)
+// console.log(a<b || a>b)
+// console.log(a>b && b<a)
+// console.log((a<b || a>b) && (a>b || b>a))
+// console.log(!(a<b || a>b))
+// console.log(a!=b)
+
+
+console.log("first")
+var number1 = 10;
+var number2 = 10;
+console.log("second")
+var number3 = 10;
+var number4 = 10;
+console.log("third")
